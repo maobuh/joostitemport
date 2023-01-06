fc2f9539fccb92d35416c3451aefbd0eb57ae87b
kwushie <christopher.ai666@gmail.com>
2023-01-06 22:36:49 +1100

    created a class for fishing shotguns + finished golden fish shotgun

--- Items/GoldenFishingGun.cs
+++ Items/GoldenFishingGun.cs
@@ -1,51 +1,53 @@
-using System;
-using Microsoft.Xna.Framework;
+using System; 
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using joostitemport.Projectiles;
+using Microsoft.Xna.Framework;
 using Terraria.DataStructures;
+using System.Collections.Generic;
+using joostitemport.Projectiles;
 
 namespace joostitemport.Items
 {
-    public class FishingGun : ModItem
+    public class GoldenFishingGun : ModItem
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Fishing Shotgun");
+            DisplayName.SetDefault("Golden Fishing Shotgun");
             Tooltip.SetDefault("Fires multiple fishing hooks");
         }
         public override void SetDefaults()
         {
             Item.width = 48;
-            Item.height = 14;
-            Item.damage = 10;
+            Item.height = 18;
+            Item.damage = 30;
             Item.DamageType = DamageClass.Ranged;
             Item.knockBack = 0;
             Item.useTime = 8;
             Item.useAnimation = 8;
             Item.useStyle = 1;
-            Item.value = 1000;
-            Item.rare = 2;
+            Item.value = 100000;
+            Item.rare = 4;
             Item.UseSound = SoundID.Item36;
             Item.autoReuse = false;
-            Item.shoot = ModContent.ProjectileType<Fishhook>();
-            Item.shootSpeed = 11f;
-            Item.fishingPole = 15;
+            //Item.shoot = 364;
+            Item.shoot = ModContent.ProjectileType<GoldenFishHook>();
+            Item.shootSpeed = 17f;
+            Item.fishingPole = 50;
         }
 
         // public override void ModifyWeaponDamage(Player player, ref float add, ref float mult, ref float flat)
         // {
         //     if (JoostMod.instance.battleRodsLoaded)
         //     {
-        //         mult *= BattleRodsFishingDamage / player.GetDamage(DamageClass.Ranged);
+        //         mult *= BattleRodsFishingDamage / player.rangedDamage;
         //     }
         // }
         // public override void GetWeaponCrit(Player player, ref int crit)
         // {
         //     if (JoostMod.instance.battleRodsLoaded)
         //     {
-        //         crit += BattleRodsCrit - player.GetCritChance(DamageClass.Ranged);
+        //         crit += BattleRodsCrit - player.rangedCrit;
         //     }
         // }
         // public float BattleRodsFishingDamage
@@ -70,18 +72,28 @@ namespace joostitemport.Items
         public override void AddRecipes()
         {
             Recipe recipe = Recipe.Create(Item.type);
-            recipe.AddIngredient(ItemID.Boomstick, 1);
-            recipe.AddIngredient(ItemID.ReinforcedFishingPole, 1);
+            recipe.AddIngredient(ItemID.GoldenFishingRod);
             recipe.AddTile(TileID.WorkBenches);
-            recipe.Register();
+
+            Recipe recipe2 = recipe.Clone();
+            Recipe recipe3 = recipe.Clone();
+
+            // recipe.AddIngredient(null, "MechanicalFishingGun");
+            // recipe.Register();
+
+            recipe2.AddIngredient(null, "FishingGun");
+            recipe2.Register();
+
+            recipe3.AddIngredient(ItemID.Boomstick);
+            recipe3.Register();
         }
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            const float spread = 45f * 0.0174f;
+            const float spread = 55f * 0.0174f;
             float baseSpeed = velocity.Length();
             float startAngle = (float) Math.Atan2(velocity.X, velocity.Y) - (spread / 2);
-            const float deltaAngle = spread / 4f;
-            for (int i = 0; i < 4; i++)
+            const float deltaAngle = spread / 10f;
+            for (int i = 0; i < 10; i++)
             {
                 float offsetAngle = startAngle + (deltaAngle * i);
                 Vector2 direction = new((float)Math.Sin(offsetAngle), (float)Math.Cos(offsetAngle));
@@ -91,4 +103,3 @@ namespace joostitemport.Items
         }
     }
 }
-
